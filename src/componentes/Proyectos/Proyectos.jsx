af7fc47d08d6577fc46b8a931fe0bd9967b12094
asierpl <asierpl210395@gmail.com>
2024-05-30 17:16:38 +0200

    enlace proyecto

--- src/componentes/Proyectos/Proyectos.jsx
+++ src/componentes/Proyectos/Proyectos.jsx
@@ -1,49 +1,38 @@
 import './Proyectos.css'
 
-
-
 let proyectos = [
     {id : 0 , href : 'https://proyecto-react-teal.vercel.app/' , title : 'Bonanza'},
-    {id : 0 , href : 'https://canonhuelva.com/' , title : 'Bonanza'},
-    {id : 0 , href : 'https://proyecto-react-teal.vercel.app/' , title : 'Bonanza'}
-    
 ]
 
 export const Proyectos = () => {
 
-
-
-    return(
-        <>
-        
+    return (
         <div className="Proyectos">
             <ul className="Proyectos-ul">
-                {proyectos.map( proyect => (
-                    <Proyect key={proyect.id} {...proyect} />
+                {proyectos.map(proyect => (
+                    <Project key={proyect.id} {...proyect} />
                 ))}
             </ul>
         </div>
-        </>
     )
 }
 
-const Proyect = (props) => {
+const Project = (props) => {
+    const {href, title} = props
 
-    const {href , title  } = props
-
-    return(
-        <>
+    return (
         <li className="Proyectos-li">
-        <a href={href} title={title} target='_blank' className="Proyectos-a">
-                <iframe 
-                    src={href} 
-                    title={title} 
-                    className="Proyectos-iframe" 
-                    loading="lazy"
-                ></iframe>
-                
+            <a href={href} title={title} target='_blank' className="Proyectos-a">
+                <div className="Proyectos-content">
+                    <iframe 
+                        src={href} 
+                        title={title} 
+                        className="Proyectos-iframe" 
+                        loading="lazy" >
+                    </iframe>
+                    <div className="Proyectos-overlay"></div>
+                </div>
             </a>
         </li>
-        </>
     )
 }
